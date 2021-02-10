944a49a32a74a11beeb369cd2db8226a17404e89
Taylor Niles <tniles320@gmail.com>
2021-02-09 16:28:25 -0800

    adding app skeleton

---
+++ client/src/utils/AUTH.js
@@ -0,0 +1,21 @@
+/* eslint-disable import/no-anonymous-default-export */
+import axios from "axios";
+
+export default {
+  // Gets user info
+  getUser: function () {
+    return axios.get("/auth/user");
+  },
+  // Logs the user out
+  logout: function () {
+    return axios.post("/auth/logout");
+  },
+  // Log the user in
+  login: function (username, password) {
+    return axios.post("/auth/login", { username, password });
+  },
+  // New user registration
+  register: function (userData) {
+    return axios.post("/auth/signup", userData);
+  },
+};
